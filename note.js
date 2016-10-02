4191dddb3738cb248e7786844c272fa0287a2584
MrZwqShuai <zwq1098621287@yahoo.com>
2016-10-02 11:59:20 +0800

    Update note.js

--- note.js
+++ note.js
@@ -1,1 +1,27 @@
-Js note
+//Js note
+<script>
+        //快速打印五分制的评分情况
+        function getRating(rating) {
+        	if(rating > 5 || rating < 0){
+        		throw new Error('数字不在范围之内') ;
+        	}
+        	return '☆☆☆☆☆★★★★★'.substring(5-rating,10-rating) ;
+        }
+        console.log(getRating(2))
+	</script>
+        
+//正确打开方式
+<script>
+        try {
+        	angular2
+        } catch (e){
+        	window.location.href = 'http://stackoverflow.com/search?q=[js]+'+e.message ;
+        }
+
+	</script>
+  //repeat
+  <script>
+        var str = new Array(n+1).join('abc') ;
+        console.log(str)
+
+	</script>
